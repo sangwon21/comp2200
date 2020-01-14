0eacdeb005e7eb7cd8e7d5935d3288aa67a0dabd
sangwon21 <junothyme@gmail.com>
2020-01-14 11:40:32 +0900

    format: 포멧팅과 전역 변수 삭제

--- Lab2/array.c
+++ Lab2/array.c
@@ -1,16 +1,14 @@
 #include <limits.h>
 #include "array.h"
 
-const int MINIMUM_INDEX = 0;
-
-int get_index_of(const int numbers[], const size_t element_count, const int num) {
+int get_index_of(const int numbers[], const size_t element_count, const int num){
     int index;
 
-    if ((int)element_count <= MINIMUM_INDEX) {
+    if ((int)element_count <= 0) {
         return -1;
     }
 
-    for (index = MINIMUM_INDEX; index < (int)element_count; index++) {
+    for (index = 0; index < (int)element_count; index++) {
         if (numbers[index] == num) {
             return index;
         }
@@ -19,14 +17,14 @@ int get_index_of(const int numbers[], const size_t element_count, const int num)
     return -1;
 }
 
-int get_last_index_of(const int numbers[], const size_t element_count, const int num) {
+int get_last_index_of(const int numbers[], const size_t element_count, const int num){
     int index;
 
-    if ((int)element_count <= MINIMUM_INDEX) {
+    if ((int)element_count <= 0) {
         return -1;
     }
 
-    for (index = (int)element_count; index >= MINIMUM_INDEX; index--) {
+    for (index = (int)element_count; index >= 0; index--) {
         if (numbers[index] == num) {
             return index;
         }
@@ -35,18 +33,18 @@ int get_last_index_of(const int numbers[], const size_t element_count, const int
     return -1;
 }
 
-int get_max_index(const int numbers[], const size_t element_count) {
+int get_max_index(const int numbers[], const size_t element_count){
     int max_index;
     int max_number;
     size_t index;
 
-    if (element_count == MINIMUM_INDEX) {
+    if (element_count == 0) {
         return -1;
     }
-    max_index = MINIMUM_INDEX;
-    max_number = numbers[MINIMUM_INDEX];
+    max_index = 0;
+    max_number = numbers[0];
 
-    for (index = MINIMUM_INDEX + 1; index < element_count; index++) {
+    for (index = 0 + 1; index < element_count; index++) {
         if (numbers[index] > max_number) {
             max_index = index;
             max_number = numbers[index];
@@ -56,18 +54,18 @@ int get_max_index(const int numbers[], const size_t element_count) {
     return max_index;
 }
 
-int get_min_index(const int numbers[], const size_t element_count) {
+int get_min_index(const int numbers[], const size_t element_count){
     int min_index;
     int min_number;
     size_t index;
 
-    if (element_count == MINIMUM_INDEX) {
+    if (element_count == 0) {
         return -1;
     }
-    min_index = MINIMUM_INDEX;
-    min_number = numbers[MINIMUM_INDEX];
+    min_index = 0;
+    min_number = numbers[0];
 
-    for (index = MINIMUM_INDEX + 1; index < element_count; index++) {
+    for (index = 0 + 1; index < element_count; index++) {
         if (numbers[index] < min_number) {
             min_index = index;
             min_number = numbers[index];
@@ -77,10 +75,10 @@ int get_min_index(const int numbers[], const size_t element_count) {
     return min_index;
 }
 
-int is_all_positive(const int numbers[], const size_t element_count) {
+int is_all_positive(const int numbers[], const size_t element_count){
     int index;
 
-    if ((int)element_count <= MINIMUM_INDEX) {
+    if ((int)element_count <= 0) {
         return FALSE;
     }
 
@@ -93,10 +91,10 @@ int is_all_positive(const int numbers[], const size_t element_count) {
     return TRUE;
 }
 
-int has_even(const int numbers[], const size_t element_count) {
+int has_even(const int numbers[], const size_t element_count){
     int index;
 
-    if ((int)element_count <= MINIMUM_INDEX) {
+    if ((int)element_count <= 0) {
         return FALSE;
     }
 
@@ -109,7 +107,7 @@ int has_even(const int numbers[], const size_t element_count) {
     return FALSE;
 }
 
-int insert(int numbers[], const size_t element_count, const int num, const size_t pos) {
+int insert(int numbers[], const size_t element_count, const int num, const size_t pos){
     
     int index;
 
@@ -131,7 +129,7 @@ int insert(int numbers[], const size_t element_count, const int num, const size_
     return TRUE;
 }
 
-int remove_at(int numbers[], const size_t element_count, const size_t index) {
+int remove_at(int numbers[], const size_t element_count, const size_t index){
 
     int pos;
 
