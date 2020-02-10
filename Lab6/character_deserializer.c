c61032c857bddb7f8266827797d1567216446767
sangwon21 <junothyme@gmail.com>
2020-02-10 21:56:50 +0900

    feat: version1용 역직렬화 함수 구현

--- Lab6/character_deserializer.c
+++ Lab6/character_deserializer.c
@@ -1,1 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
 #include "character_deserializer.h"
+#define BUFFER_LENGTH 255
+
+int my_atoi(const char* number)
+{
+    int result = 0;
+    const char* pointer = number;
+
+    while (*pointer != '\0') {
+        result = result * 10 + (*pointer - '0');
+        pointer++;
+    }
+
+    return result;
+}
+
+void version1_deserialize(character_v3_t* out_character)
+{
+    char buffer[BUFFER_LENGTH] = "player_";
+    char tmp[BUFFER_LENGTH];
+    const char* token;
+
+    while (token) {
+        if (strcmp("id", token) == 0) {
+            token = strtok(NULL, ":,");
+            strcat(tmp, token);
+            strcpy(out_character->name, tmp);
+        } else if (strcmp("lvl", token) == 0) {
+            token = strtok(NULL, ":,");
+            out_character->level = my_atoi(token);
+        } else if (strcmp("str", token) == 0) {
+            token = strtok(NULL, ":,");
+            out_character->strength = my_atoi(token);
+        } else if (strcmp("dex", token) == 0) {
+            token = strtok(NULL, ":,");
+            out_character->dexterity = my_atoi(token);
+        } else if (strcmp("intel", token) == 0) {
+            token = strtok(NULL, ":,");
+            out_character->intelligence = my_atoi(token);
+        } else if (strcmp("def", token) == 0) {
+            token = strtok(NULL, ":,");
+            out_character->armour = my_atoi(token);
+        } else if (strcmp("hp", token) == 0) {
+            token = strtok(NULL, ":,");
+            out_character->health = my_atoi(token);
+        } else if (strcmp("mp", token) == 0) {
+            token = strtok(NULL, ":,");
+            out_character->mana = my_atoi(token);
+        }
+        token = strtok(NULL, ":,");
+    }
+    out_character->minion_count = 0;
+    out_character->evasion = 10;
+    out_character->leadership = 1;
+    out_character->elemental_resistance.fire = 1;
+    out_character->elemental_resistance.cold = 1;
+    out_character->elemental_resistance.lightning = 1;
+}
+
+int get_character(const char* filename, character_v3_t* out_character)
+{
+    char buffer[BUFFER_LENGTH];
+    char tmp[BUFFER_LENGTH] = "player_";
+    char* token;
+    FILE* file = fopen(filename, "r");
+
+    fscanf(file, "%s", buffer);
+    
+    token = strtok(buffer, ":,");
+
+    
+    
+    fclose(file);
+    return 0;
+}
--- Lab6/main.c
+++ Lab6/main.c
@@ -1,4 +1,13 @@
+#include "character_deserializer.h"
+#include <stdio.h>
+
 int main(void)
 {
+    character_v3_t test;
+    get_character("Superman_v1.txt", &test);
+
+    printf("name is %s\n level is %d\n strength is %d\n", test.name, test.level, test.strength);
+    printf("dexterity is %d\n intelligence is %d\n armours is %d\n", test.dexterity, test.intelligence, test.armour);
+    printf("hp is %d\n mp is %d\n", test.health, test.mana);   
     return 0;
 }
