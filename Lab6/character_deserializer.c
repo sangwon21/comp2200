d151b916170ff5135f4c11b69f64e27c1517a52e
sangwon21 <junothyme@gmail.com>
2020-02-12 23:13:18 +0900

    feat: version3 역직렬화 함수 작성

--- Lab6/character_deserializer.c
+++ Lab6/character_deserializer.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 #include "character_deserializer.h"
-#define BUFFER_LENGTH 255
-#define NAME_LIMIT 50
+#define BUFFER_LENGTH (512)
+#define NAME_LIMIT (50)
+#define TRUE (1)
+#define FALSE (0)
 
 int my_atoi(const char* number)
 {
@@ -69,6 +71,7 @@ void version2_deserialize(character_v3_t* out_character, char* buffer)
     
     while (token) {
         if (count == 0) {
+            strncpy(out_character->name, player_name, NAME_LIMIT);
             goto increase_token;
         }
         if (count == 1) {
@@ -98,26 +101,104 @@ increase_token:
     }
 }
 
+void read_minions(character_v3_t* out_character, FILE* file)
+{
+    char buffer[BUFFER_LENGTH];
+    char line[BUFFER_LENGTH];
+    char player_name[NAME_LIMIT];
+    char space;
+    int count = 0;
+    
+    while (1) {
+        if (fgets(line, BUFFER_LENGTH, file) == NULL) {
+            clearerr(file);
+            break;
+        }
+        if (sscanf(line, "%s %c %d %c %d %c %d", buffer, &space, &out_character->minions[count].health, &space, &out_character->minions[count].strength, &space, &out_character->minions[count].defence)) {
+                strncpy(player_name, buffer, NAME_LIMIT);
+                strncpy(out_character->minions[count].name, player_name, NAME_LIMIT);                            
+                count++;
+        }
+    }
+    
+}
+
+void version3_deserialize(character_v3_t* out_character, char* buffer, FILE* file)
+{
+    char player_name[NAME_LIMIT];
+    char* token = strtok(buffer, " |");
+    int count = 0;
+    strncpy(player_name, token, NAME_LIMIT);
+
+    while (token) {
+        if (count == 0) {
+            strncpy(out_character->name, player_name, NAME_LIMIT);
+            goto increase_token;
+        }
+        if (count == 1) {
+            out_character->level = my_atoi(token);
+        } else if (count == 2) {
+            out_character->health= my_atoi(token);
+        } else if (count == 3) {
+            out_character->mana = my_atoi(token);
+        } else if (count == 4) {
+            out_character->strength = my_atoi(token);
+        } else if (count == 5) {
+            out_character->dexterity = my_atoi(token);
+        } else if (count == 6) {
+            out_character->intelligence = my_atoi(token);
+        }  else if (count == 7) {
+            out_character->armour = my_atoi(token); 
+        } else if (count == 8) {
+            out_character->evasion = my_atoi(token);
+        } else if (count == 9) {
+            out_character->elemental_resistance.fire = my_atoi(token);
+        } else if (count == 10) {
+            out_character->elemental_resistance.cold = my_atoi(token);
+        } else if (count == 11) {
+            out_character->elemental_resistance.lightning = my_atoi(token);
+        } else if (count == 12) {
+            out_character->leadership = my_atoi(token);
+        } else if (count == 13) {
+            out_character->minion_count = my_atoi(token);
+
+            if (out_character->minion_count != 0) {
+                fgets(buffer, BUFFER_LENGTH, file);
+                printf("buffer is %s\n", buffer);
+                read_minions(out_character, file);
+            }
+        }
+increase_token:
+        count++;
+        token = strtok(NULL, " |");
+    }
+}
+
 int get_character(const char* filename, character_v3_t* out_character)
 {
     char buffer[BUFFER_LENGTH];
     char* token;
     FILE* file = fopen(filename, "r");
 
-    fscanf(file, "%s", buffer);
-    
+    fgets(buffer, BUFFER_LENGTH, file);
+
     token = strstr(buffer, ":");
     if (token != NULL) {
         version1_deserialize(out_character, buffer);
+        goto file_end;
     }
 
     token = strstr(buffer, ",");
     if(token != NULL) {
-        fscanf(file, "%s", buffer);
+        fgets(buffer, BUFFER_LENGTH, file);
         version2_deserialize(out_character, buffer);
+        goto file_end;
     }
+
+    fgets(buffer, BUFFER_LENGTH, file);
+    version3_deserialize(out_character, buffer, file);
     
-    
+file_end:
     fclose(file);
     return 0;
 }
--- Lab6/main.c
+++ Lab6/main.c
@@ -5,11 +5,12 @@
 int main(void)
 {
     character_v3_t test;
-    get_character("Batman_v2.txt", &test);
+    get_character("Wonderwoman_v3.txt", &test);
 
-    printf("name is %s\n level is %d\n strength is %d\n", test.name, test.level, test.strength);
-    printf("dexterity is %d\n intelligence is %d\n armours is %d\n", test.dexterity, test.intelligence, test.armour);
-    printf("hp is %d\n mp is %d\n", test.health, test.mana);   
+    printf("name is %s\nlevel is %d\nstrength is %d\n", test.name, test.level, test.strength);
+    printf("dexterity is %d\nintelligence is %d\narmours is %d\n", test.dexterity, test.intelligence, test.armour);
+    printf("hp is %d\nmp is %d\n", test.health, test.mana);   
+    printf("minion1 is name %s\nminion2 name is %s\nminion3 name is %s", test.minions[0].name, test.minions[1].name, test.minions[2].name);   
     // char test[123] = "I love you!!";
     // char* token  = strstr(test, ",");
     // printf("%s", token);
