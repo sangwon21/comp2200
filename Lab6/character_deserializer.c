dee7b032a65805373b9b8500d401e61dcfb70b8f
sangwon21 <junothyme@gmail.com>
2020-02-13 00:37:09 +0900

    feat: count변수를 활용한 input 입력에서 sscanf를 활용한 input 입력으로 교체

--- Lab6/character_deserializer.c
+++ Lab6/character_deserializer.c
@@ -55,11 +55,11 @@ void version1_deserialize(character_v3_t* out_character, char* buffer)
         token = strtok(NULL, ":,");
     }
     out_character->minion_count = 0;
-    out_character->evasion = 10;
-    out_character->leadership = 1;
-    out_character->elemental_resistance.fire = 1;
-    out_character->elemental_resistance.cold = 1;
-    out_character->elemental_resistance.lightning = 1;
+    out_character->evasion = out_character->dexterity / 2;
+    out_character->leadership = out_character->level / 10;
+    out_character->elemental_resistance.fire = (out_character->armour / 4) / 3;
+    out_character->elemental_resistance.cold = (out_character->armour / 4) / 3;
+    out_character->elemental_resistance.lightning = (out_character->armour / 4) / 3;
 }
 
 void version2_deserialize(character_v3_t* out_character, char* buffer)
@@ -68,7 +68,7 @@ void version2_deserialize(character_v3_t* out_character, char* buffer)
     const char* token = strtok(buffer, ",");
     int count = 0;
     strncpy(player_name, token, NAME_LIMIT);
-    
+
     while (token) {
         if (count == 0) {
             strncpy(out_character->name, player_name, NAME_LIMIT);
@@ -99,6 +99,8 @@ increase_token:
         count++;
         token = strtok(NULL, ",");
     }
+    out_character->leadership = out_character->level / 10;
+    out_character->minion_count = 0;
 }
 
 void read_minions(character_v3_t* out_character, FILE* file)
@@ -108,69 +110,47 @@ void read_minions(character_v3_t* out_character, FILE* file)
     char player_name[NAME_LIMIT];
     char space;
     int count = 0;
-    
-    while (1) {
+
+    while (TRUE) {
         if (fgets(line, BUFFER_LENGTH, file) == NULL) {
             clearerr(file);
             break;
         }
+
         if (sscanf(line, "%s %c %d %c %d %c %d", buffer, &space, &out_character->minions[count].health, &space, &out_character->minions[count].strength, &space, &out_character->minions[count].defence)) {
                 strncpy(player_name, buffer, NAME_LIMIT);
                 strncpy(out_character->minions[count].name, player_name, NAME_LIMIT);                            
                 count++;
         }
-    }
-    
+    } 
 }
 
-void version3_deserialize(character_v3_t* out_character, char* buffer, FILE* file)
+void version3_deserialize(character_v3_t* out_character, FILE* file)
 {
     char player_name[NAME_LIMIT];
-    char* token = strtok(buffer, " |");
-    int count = 0;
-    strncpy(player_name, token, NAME_LIMIT);
+    char buffer[BUFFER_LENGTH];
+    char line[BUFFER_LENGTH];
+    char space;
+    int resistance;
 
-    while (token) {
-        if (count == 0) {
-            strncpy(out_character->name, player_name, NAME_LIMIT);
-            goto increase_token;
+    while (TRUE) {
+        if (fgets(line, BUFFER_LENGTH, file) == NULL) {
+            clearerr(file);
+            break;
         }
-        if (count == 1) {
-            out_character->level = my_atoi(token);
-        } else if (count == 2) {
-            out_character->health= my_atoi(token);
-        } else if (count == 3) {
-            out_character->mana = my_atoi(token);
-        } else if (count == 4) {
-            out_character->strength = my_atoi(token);
-        } else if (count == 5) {
-            out_character->dexterity = my_atoi(token);
-        } else if (count == 6) {
-            out_character->intelligence = my_atoi(token);
-        }  else if (count == 7) {
-            out_character->armour = my_atoi(token); 
-        } else if (count == 8) {
-            out_character->evasion = my_atoi(token);
-        } else if (count == 9) {
-            out_character->elemental_resistance.fire = my_atoi(token);
-        } else if (count == 10) {
-            out_character->elemental_resistance.cold = my_atoi(token);
-        } else if (count == 11) {
-            out_character->elemental_resistance.lightning = my_atoi(token);
-        } else if (count == 12) {
-            out_character->leadership = my_atoi(token);
-        } else if (count == 13) {
-            out_character->minion_count = my_atoi(token);
-
-            if (out_character->minion_count != 0) {
-                fgets(buffer, BUFFER_LENGTH, file);
-                printf("buffer is %s\n", buffer);
-                read_minions(out_character, file);
-            }
+        
+        if (sscanf(line, "%s %c %d %c %d %c %d %c %d %c %d %c %d %c %d %c %d %c %d %c %d %c %d %c %d %c %d27", buffer, &space, &out_character->level, &space, &out_character->health, &space, 
+        &out_character->mana, &space, &out_character->strength, &space, &out_character->dexterity, &space, &out_character->intelligence, &space, &out_character->armour, 
+        &space, &out_character->evasion, &space, &out_character->elemental_resistance.fire, &space, &out_character->elemental_resistance.cold,
+        &space, &out_character->elemental_resistance.lightning, &space, &out_character->leadership, &space, &out_character->minion_count)) {
+                strncpy(player_name, buffer, NAME_LIMIT);
+                strncpy(out_character->name, player_name, NAME_LIMIT);
+                
+                if (out_character->minion_count != 0) {
+                    fgets(buffer, BUFFER_LENGTH, file);
+                    read_minions(out_character, file);
+                }                            
         }
-increase_token:
-        count++;
-        token = strtok(NULL, " |");
     }
 }
 
@@ -178,6 +158,7 @@ int get_character(const char* filename, character_v3_t* out_character)
 {
     char buffer[BUFFER_LENGTH];
     char* token;
+    int version = 1;
     FILE* file = fopen(filename, "r");
 
     fgets(buffer, BUFFER_LENGTH, file);
@@ -185,6 +166,7 @@ int get_character(const char* filename, character_v3_t* out_character)
     token = strstr(buffer, ":");
     if (token != NULL) {
         version1_deserialize(out_character, buffer);
+        version = 1;
         goto file_end;
     }
 
@@ -192,13 +174,14 @@ int get_character(const char* filename, character_v3_t* out_character)
     if(token != NULL) {
         fgets(buffer, BUFFER_LENGTH, file);
         version2_deserialize(out_character, buffer);
+        version = 2;
         goto file_end;
     }
 
-    fgets(buffer, BUFFER_LENGTH, file);
-    version3_deserialize(out_character, buffer, file);
-    
+    version3_deserialize(out_character, file);
+    version = 3;
+
 file_end:
     fclose(file);
-    return 0;
+    return version;
 }
