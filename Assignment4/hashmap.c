e9b18b0f0805695bce09630af9266ade019f4279
sangwon21 <junothyme@gmail.com>
2020-03-16 12:08:11 +0900

    feat: destroy 함수 제작

--- Assignment4/hashmap.c
+++ Assignment4/hashmap.c
@@ -11,3 +11,23 @@ hashmap_t* init_hashmap_malloc(size_t length, int (*p_hash_func)(const char* key
 
 	return hashmap;
 }
+
+void destroy(hashmap_t* hashmap)
+{
+    int plist_index = 0;
+    if (hashmap == NULL) {
+        return;
+    }
+
+    if (hashmap->plist == NULL) {
+        free(hashmap);
+        return;
+    }
+
+    for (plist_index = 0; plist_index < hashmap->length; plist_index++) {
+        free(hashmap->plist[plist_index]);
+    }
+
+    free(hashmap->plist);
+    free(hashmap);
+}
