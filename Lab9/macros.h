d1e31a6b86f233ae691ebf539f81ef2448e579de
sangwon21 <junothyme@gmail.com>
2020-03-16 11:04:54 +0900

    fix: for문과 while문 사용시 (괄호) 제거
    why: (괄호)를 제거하지 않으면 for문이나 while문이 괄호로 묶여서 loop를 실행할 수 없게 된다.

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -1,28 +1,22 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-#define MAX(x, y) ( if (x > y) { \
-                        return x; \
-                    } \
-                    return y; \
-                  )
+#define MAX(x, y) ((x > y) ? (x) : (y)) 
 
-#define MIN(x, y) ( if (x < y) { \
-                        return y; \
-                    } \
-                    return x; \
-                  )
+#define MIN(x, y) ((x < y) ? (x) : (y))
 
-#define ABS(x) ( if (x < 0) { \
-                    return x * -1; \
-                } \
-                return x;\
-                )
+#define ABS(x) ((x < 0) ? (x * -1) : x)
 
-#define RANGE(curr, min, max) (for (curr = min; curr < max + 1; curr++))
+#define RANGE(curr, min, max) for (curr = min; curr < max + 1; curr++)
 
-#define RANGE_DESC(curr, max, min) (for (curr = max; curr > min - 1; curr--))
+#define RANGE_DESC(curr, max, min) for (curr = max; curr > min - 1; curr--)
 
-#define SET(ary, start, count, value)
+#define SET(ary, start, count, value) { \
+                                        int index = start; \
+                                        for (; index < start + count; index++) { \
+                                            ary[index] = value; \
+                                        } \
+                                      } \
+                                      
 
 #endif /* MACROS_H */
