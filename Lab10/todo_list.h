c8cca535090ad7032a482191dad0810a0ae4af6c
sangwon21 <junothyme@gmail.com>
2020-03-23 10:26:03 +0900

    feat: 기본 파일 설정
    - header 파일 작성.

---
+++ Lab10/todo_list.h
@@ -0,0 +1,28 @@
+#ifndef TODO_LIST_H
+#define TODO_LIST_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct todo_list {
+    // Make your own members
+    int dummy;
+    int priority;
+    char* todo;
+} todo_list_t;
+
+todo_list_t* init_todo_list_malloc(size_t max_size);
+
+void dispose(todo_list_t* todo_list);
+
+bool add_todo(todo_list_t* todo_list, const int32_t priority, const char*   task);
+
+bool complete_todo(todo_list_t* todo_list);
+
+const char* peek_or_null(todo_list_t* todo_list);
+
+size_t get_count(todo_list_t* todo_list);
+
+bool is_empty(todo_list_t* todo_list);
+
+#endif /* TODO_LIST_H */
