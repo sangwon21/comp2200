f0b365ed2a12407e91c4bcffd37b58e45f3cae83
sangwon21 <junothyme@gmail.com>
2020-03-30 17:57:42 +0900

    feat: 과제 명세서 요구 함수 작성

---
+++ Lab11/data_store.c
@@ -0,0 +1,124 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include "data_store.h"
+
+user_t* get_user_by_id_or_null(user_t** users_or_null, size_t id)
+{
+    if (users_or_null == NULL) {
+        return NULL;
+    }
+
+    user_t** user_ptr = users_or_null;
+
+    while (*user_ptr != NULL) {
+        if ((*user_ptr)->id == id) {
+            return *user_ptr;
+        }
+        user_ptr++;
+    }
+
+    return NULL;
+}
+
+user_t* get_user_by_username_or_null(user_t** users_or_null, const char* username)
+{
+    if (users_or_null == NULL) {
+        return NULL;
+    }
+
+    user_t** user_ptr = users_or_null;
+    while (*user_ptr != NULL) {
+        if (strcmp((*user_ptr)->username, username) == 0) {
+            return *user_ptr;
+        }
+        user_ptr++;
+    }
+    return NULL;
+}
+
+char* apply_rule(char* target)
+{
+    int length = strlen(target);
+    if (length == 1) {
+        target[length - 1] = '*';
+        return target;
+    }
+
+    if (length == 2) {
+        target[length - 1] = '*';
+        return target;
+    }
+
+    for (int i = 0; i < length; i++) {
+        if (i == 0 || i == length - 1) {
+            continue;
+        }
+        target[i] = '*';
+    }
+
+    return target;    
+}
+
+char* convert_email(char* email)
+{
+    char* ptr = strtok(email, "@");
+    int length = strlen(ptr);
+    apply_rule(ptr);
+    email[length] = '@';
+
+    return email;
+}
+
+char* convert_password(char* password)
+{
+    apply_rule(password);
+    return password;
+}
+
+bool update_email(user_t** users_or_null, size_t id, const char* email)
+{
+    user_t* target_user = get_user_by_id_or_null(users_or_null, id);
+    if (target_user == NULL) {
+        return false;
+    }
+
+    FILE* file = fopen("log.txt", "a");
+    char old_email[51];
+    strcpy(old_email, target_user->email);
+    char* new_email = (char*)email;
+    strcpy(target_user->email, new_email);
+
+#ifdef RELEASE
+    convert_email(old_email);
+    convert_email(new_email);
+#endif
+    fprintf(file, "TRACE: User %d updated email from \"%s\" to \"%s\"\n", target_user->id, old_email, new_email);
+    fclose(file);
+    return true;
+}
+
+bool update_password(user_t** users_or_null, size_t id, const char* password)
+{
+    user_t* target_user = get_user_by_id_or_null(users_or_null, id);
+    if (target_user == NULL) {
+        return false;
+    }
+
+    FILE* file = fopen("log.txt", "a");
+    char old_password[51];
+    strcpy(old_password, target_user->password);
+    char* new_password = (char*)password;
+    strcpy(target_user->password, new_password);
+    
+#ifdef RELEASE
+    convert_password(old_password);
+    convert_password(new_password);
+#endif
+    fprintf(file, "TRACE: User %d updated email from \"%s\" to \"%s\"\n", target_user->id, old_password, new_password);
+    fclose(file);
+    return true;
+}
---
+++ Lab11/user.h
@@ -0,0 +1,12 @@
+#ifndef USER_H
+#define USER_H
+
+typedef struct user
+{
+    size_t id;
+    char username[51];
+    char password[51];
+    char email[51];
+} user_t;
+
+#endif /* USER_H */
