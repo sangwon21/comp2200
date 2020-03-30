459119e458889c86ab93ef636c8aec6d9fe0c0bd
sangwon21 <junothyme@gmail.com>
2020-03-30 21:46:30 +0900

    fix: 헤더 파일 정리

--- Lab11/data_store.h
+++ Lab11/data_store.h
@@ -9,16 +9,8 @@ user_t* get_user_by_id_or_null(user_t** users_or_null, size_t id);
 
 user_t* get_user_by_username_or_null(user_t** users_or_null, const char* username);
 
-char* convert_email(char* email);
-
 bool update_email(user_t** users_or_null, size_t id, const char* email);
 
 bool update_password(user_t** users_or_null, size_t id, const char* password);
-
-static char* convert_password(char* password);
-
-static char* convert_email(char* email);
-
-static char* apply_rule(char* target);
 
 #endif /* DATA_STORE_H */
